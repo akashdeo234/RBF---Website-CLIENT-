c0b3ddf60b98440313654e0038fff85f579d3fdf
Akash <akashstunes@gmail.com>
2021-10-15 09:58:30 +0530

    added landing page

--- js-files/common.js
+++ js-files/common.js
@@ -13,3 +13,35 @@ handburger.addEventListener('click',()=>{
     )
 
 })
+
+
+//swiper
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    effect: 'cube',
+    
+
+    cubeEffect: {
+        slideShadows: false,
+        shadow: false,
+      },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
